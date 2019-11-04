f279de61e3ce6893838a1c0496c3839453da04f3
swimming_kim <tge1375@naver.com>
2019-11-04 15:42:06 +0900

    developed at 11/3

--- App.js
+++ App.js
@@ -1,10 +1,93 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  TextInput,
+  Dimensions,
+  Platform,
+  ScrollView
+} from 'react-native';
+import Todo from './Todo';
+import { AppLoading } from 'expo';
+import uuidv1 from 'uuid/v1';
 
-export default function App() {
+const { width, height } = Dimensions.get('window');
+
+const App = () => {
+  const [newTodo, setNewTodo] = useState('');
+  const [loading, setLoading] = useState(true);
+  const [todos, setTodos] = useState({});
+
+  const onChangeText = (text) => {
+    setNewTodo(text);
+  }
+  const loadTodos = () => {
+    // loading in here
+    setLoading(false);
+  }
+  const addTodo = () => {
+    if (newTodo !== '') {
+      console.log(newTodo);
+      const id = uuidv1();
+      const tempTodo = {
+        [id]: {
+          id,
+          complete: false,
+          text: newTodo,
+          createdAt: Date.now()
+        }
+      }
+      setTodos(currentTodos => {
+        return { ...currentTodos, ...tempTodo }
+      });
+      setNewTodo('');
+    }
+  }
+  const deleteTodo = id => {
+    setTodos(currentTodo => {
+      const tempTodo = currentTodo;
+      delete tempTodo[id];
+      return { ...tempTodo }
+    });
+  }
+
+  useEffect(() => {
+    loadTodos();
+  }, [])
+
+  if (loading) {
+    return <AppLoading />
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar barStyle='light-content'></StatusBar>
+      <Text style={styles.title}>Kawai Todo!</Text>
+      <View style={styles.card}>
+        <TextInput
+          value={newTodo}
+          style={styles.input}
+          placeholder='할 일...'
+          placeholderTextColor="#999"
+          onChangeText={onChangeText}
+          returnKeyType={"done"}
+          autoCorrect={false}
+          onSubmitEditing={addTodo}>
+        </TextInput>
+        <ScrollView contentContainerStyle={styles.todos}>
+          {Object.values(todos).map(todo => 
+            <Todo 
+              key={todo.id} 
+              {...todo}
+              isComplete={todo.complete}
+              deleteTodo={deleteTodo}
+            />
+          )}
+          <Text>{JSON.stringify(todos)}</Text>
+        </ScrollView>
+      </View>
     </View>
   );
 }
@@ -12,8 +95,48 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFD939', //나중에 gradient로 바꿉시다. ㅇㅇㅇ 
     alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'center',
   },
+  title: {
+    color: '#353839',
+    fontSize: 40,
+    marginTop: 57,
+    fontWeight: '500',
+    marginBottom: 20,
+  },
+  card: {
+    backgroundColor: 'white',
+    width: width - 30,
+    flex: 1,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    ...Platform.select({
+      ios: {
+        shadowColor: 'rgb(50, 50, 50)',
+        shadowOpacity: .5,
+        shadowRadius: 5,
+        shadowOffset: {
+          width: 0,
+          height: -1
+        }
+      },
+      android: {
+        elevation: 9
+      }
+    })
+  },
+  input: {
+    padding: 20,
+    fontSize: 20,
+    borderBottomColor: '#bbb',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  todos: {
+    alignItems: 'center',
+  }
 });
+
+
+export default App;
---
+++ Todo.js
@@ -0,0 +1,162 @@
+import React, { useState, useCallback } from 'react'
+import {
+    Text,
+    View,
+    TouchableOpacity,
+    StyleSheet,
+    Dimensions,
+    TextInput
+} from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import PropTypes from 'prop-types';
+//checkbox-blank-outline
+
+const { width, height } = Dimensions.get('window');
+const CIRCLE_RADIUS = 16;
+const ICON_SIZE = 27;
+const MAX_TODO_LEN = 14;
+
+const Todo = ({ id, text, isComplete, deleteTodo }) => {
+    const [editMode, setEditMode] = useState(false);
+    const [complete, setComplete] = useState(isComplete);
+    const [editValue, setEditValue] = useState(text);
+
+    const toggleComplete = useCallback(
+        () => {
+            // console.log('dfdf');
+            setComplete(complete => !complete);
+        },
+        [complete],
+    );
+    const startEditting = () => {
+        // setEditValue(text);
+        setEditMode(true);
+    }
+    const endEditting = () => {
+        // setEditValue()
+        setEditMode(false);
+    }
+    const onChangeText = (text) => {
+        setEditValue(text);
+    }
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.column}>
+                <TouchableOpacity onPress={toggleComplete}>
+                    {/* <MaterialCommunityIcons name='checkbox-blank-outline' size={32}></MaterialCommunityIcons> */}
+                    <View style={[
+                        styles.circle,
+                        complete ? styles.completedCircle : styles.uncompltedCircle
+                    ]}/>
+                </TouchableOpacity>
+                {editMode ?
+                    (<TextInput
+                        value={editValue}
+                        multiline={true}
+                        style={[
+                            styles.input,
+                            styles.text,
+                            complete ? styles.completedText : styles.uncompltedText
+                        ]}
+                        onChangeText={onChangeText}
+                        returnKeyType={'done'}
+                        onBlur={endEditting}
+                    />
+                    ) : (
+                    <Text
+                        style={[
+                            styles.text, 
+                            complete ? styles.completedText : styles.uncompltedText
+                            ]}>
+                        {editValue.length > MAX_TODO_LEN ? editValue.slice(0, MAX_TODO_LEN) + '...' : editValue}
+                    </Text>
+                    )}
+            </View>
+            {editMode ?
+                (<View style={styles.actions}>
+                    <TouchableOpacity onPress={endEditting}>
+                        <View style={styles.actionContainer}>
+                            <MaterialCommunityIcons name='checkbox-marked' size={ICON_SIZE} />
+                        </View>
+                    </TouchableOpacity>
+                </View>) :
+                (<View style={styles.actions}>
+                    <TouchableOpacity onPress={startEditting}>
+                        <View style={styles.actionContainer}>
+                            <MaterialCommunityIcons name='pencil-box-outline' size={ICON_SIZE} color='#900B3D' />
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => deleteTodo(id)}>
+                        <View style={styles.actionContainer}>
+                            <MaterialCommunityIcons name='delete' size={ICON_SIZE} color='#571745' />
+                        </View>
+                    </TouchableOpacity>
+                </View>)}
+        </View>
+    )
+}
+
+Todo.propTypes = {
+    id: PropTypes.string.isRequired,
+    isComplete: PropTypes.bool.isRequired,
+    text: PropTypes.string.isRequired,
+    deleteTodo: PropTypes.func.isRequired
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: width - 60,
+        borderBottomColor: '#bbb',
+        borderBottomWidth: StyleSheet.hairlineWidth,
+        flexDirection: 'row',
+        alignItems: 'center',
+        // backgroundColor: 'black'
+    },
+    text: {
+        fontSize: 23,
+        fontWeight: '100',
+        marginVertical: 12,
+        marginLeft: 5,
+    },
+    completedText: {
+        color: '#bbb',
+        textDecorationLine: 'line-through'
+    },
+    uncompltedText: {
+        color: '#353839'
+    },
+    circle: {
+        width: CIRCLE_RADIUS * 2,
+        height: CIRCLE_RADIUS * 2,
+        borderRadius: CIRCLE_RADIUS,
+        backgroundColor: 'white',
+        borderWidth: 4,
+    },
+    completedCircle: {
+        borderColor: '#bbb',
+    },
+    uncompltedCircle: {
+        borderColor: '#FE8C3B',
+    },
+    column: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        width: width / 2,
+    },
+    actions: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+    },
+    actionContainer: {
+        marginVertical: 10,
+        marginHorizontal: 6,
+        // paddingHorizontal: 5
+    },
+    input: {
+        width: ( width / 2 ) + 10,
+    }
+})
+
+export default Todo;
